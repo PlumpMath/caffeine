5dc5de05683e706b4c1a8b320470a19f048b47f5
Matthew Banister <matthew.banister@gmail.com>
2015-09-29 12:45:42 -0400

    Convert functions to pipelines and compose

--- src/caffeine/core.clj
+++ src/caffeine/core.clj
@@ -6,46 +6,66 @@
 
 (defn froth-milk
   [milk]
-  (println "Frothing milk...")
-  (Thread/sleep (rand 3000))
-  "frothed milk")
+  (let [out (chan)]
+    (go
+      (when (<! milk)
+        (println "Frothing milk...")
+        (Thread/sleep (rand 3000))
+        (>! out "frothed milk")))
+    out))
 
 (defn heat-water
   [water]
-  (println "Heating water...")
-  (Thread/sleep (rand 3000))
-  "boiled water")
+  (let [out (chan)]
+    (go
+      (when (<! water)
+        (println "Heating water...")
+        (Thread/sleep (rand 3000))
+        (>! out "boiled water")))
+    out))
 
 (defn grind-coffee
   [beans]
-  (println "Grinding beans manually...")
-  (Thread/sleep (rand 3000))
-  "coffee grounds")
+  (let [out (chan)]
+    (go
+      (when (<! beans)
+        (println "Grinding beans manually...")
+        (Thread/sleep (rand 3000))
+        (>! out "ground coffee")))
+    out))
 
 (defn pour-water
   [ground-coffee hot-water]
-  (println "Pouring water...")
-  (Thread/sleep (rand 3000))
-  "hot coffee")
+  (let [out (chan)]
+    (go
+      (when (and (<! ground-coffee) (<! hot-water))
+        (println "Pouring water...")
+        (Thread/sleep (rand 3000))
+        (>! out "hot coffee")))
+    out))
 
 (defn combine
   [foamed-milk brewed-coffee]
-  (println "Combining...")
-  (Thread/sleep (rand 3000))
-  "a delicious latte")
+  (let [out (chan)]
+    (go
+      (when (and (<! foamed-milk) (<! brewed-coffee))
+        (println "Combining...")
+        (Thread/sleep (rand 3000))
+        (>! out "a delicious latte")))
+    out))
 
 (defn main []
-  (let [milk-input "Cold Milk"
-        beans-input "Coffee Beans"
-        water-input "Cold Water"
-        ch1 (chan)
-        ch2 (chan)
-        ch3 (chan)
-        ch4 (chan)
-        ch5 (chan)]
-    (go (>! ch1 (froth-milk milk-input)))
-    (go (>! ch2 (heat-water water-input)))
-    (go (>! ch3 (grind-coffee beans-input)))
-    (go (>! ch4 (pour-water (<! ch3) (<! ch2))))
-    (go (>! ch5 (combine (<! ch1) (<! ch4))))
-    (println (<!! ch5))))
+  (let [milk-input (chan)
+        beans-input (chan)
+        water-input (chan)
+        output
+        (combine
+         (pour-water
+          (grind-coffee beans-input)
+          (heat-water water-input))
+         (froth-milk milk-input))]
+    (go
+      (>! milk-input "Milk")
+      (>! beans-input "Coffee Beans")
+      (>! water-input "Cold Water"))
+    (println (<!! output))))
